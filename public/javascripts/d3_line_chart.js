83cb9dfb528cf829b1b70465decf870ec16dcc91
Isernhagen, Seth <sisernhagen@gmail.com>
2015-03-25 13:33:07 -0700

    adding symbol dropdown

--- public/javascripts/d3_line_chart.js
+++ public/javascripts/d3_line_chart.js
@@ -14,7 +14,8 @@ var y = d3.scale.linear().range([height, 0]);
 
 // Define the axes
 var xAxis = d3.svg.axis().scale(x)
-    .orient("bottom").ticks(5);
+    .orient("bottom").ticks(5)
+    .tickFormat(d3.time.format("%d-%b-%y"));
 
 var yAxis = d3.svg.axis().scale(y)
     .orient("left").ticks(5);
---
+++ public/javascripts/symbol_dropdown.js
@@ -0,0 +1,16 @@
+﻿$("#symbol-ul li").click(function () {
+    
+    var timeValue = $(this).text() + '&nbsp<span class="caret"></span>';
+    
+    $("#symbol-button:first-child").html(timeValue);
+    
+    var tmpSelected = $(this).attr('data-value');
+    
+    //selected value changed, filter chart
+    if (currentTimeOption != tmpSelected) {
+        currentTimeOption = tmpSelected;
+        
+        //filter data using given time value
+        FilterData(currentCopyNumberOption, getStartDate(currentTimeOption), getEndDate(currentTimeOption), currentBucketOption);
+    }
+});
